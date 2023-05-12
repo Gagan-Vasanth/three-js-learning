b0b63c230a06d1bbd1a54426317d352f2d66ac29
Gagan Vasanth <mynameisgagan20@gmail.com>
2023-05-12 13:57:44 +0530

    [camera-scene-renderer] updated the client.ts file to test the different cameras and renderers using canvas and without canvas

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,19 +1,30 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"; // orbit controls are used to control the entire object which is running on the browser
 
-const scene = new THREE.Scene();
+const scene = new THREE.Scene(); // we can create n number of scenes
+
+// changing the background of the screen;
+// scene.background = new THREE.Color(0xffffff);
 
 const camera = new THREE.PerspectiveCamera(
   75,
-  window.innerWidth / window.innerHeight,
-  0.2,
-  1000
+  1,
+  0.1,
+  10
 );
 camera.position.z = 2;
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+const camera2 = new THREE.OrthographicCamera(-2, 2, 2, -2);
+camera2.position.x = 1;
+
+camera2.lookAt(new THREE.Vector3(0, 0, 0));
+const renderer = new THREE.WebGLRenderer();  // we have to create a renderer which will create a canvas tag in the index.html
+renderer.setSize(400, 400);
 document.body.appendChild(renderer.domElement);
+
+const renderer2 = new THREE.WebGL1Renderer();
+renderer2.setSize(300, 300);
+document.body.appendChild(renderer2.domElement);
 
 /* 
 just write your orbit controls method with a new operator and 
@@ -21,6 +32,8 @@ pass the camera and dom element that you want to control at UI
 */
 
 new OrbitControls(camera, renderer.domElement);
+new OrbitControls(camera2, renderer2.domElement);
+
 
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({
@@ -49,7 +62,8 @@ function animate() {
 }
 
 function render() {
-  renderer.render(scene, camera);
+    renderer.render(scene, camera);
+    renderer2.render(scene, camera2);
 }
 
 animate();

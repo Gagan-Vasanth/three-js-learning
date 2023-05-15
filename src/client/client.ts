9c7280b289ba03aaa0bca7ae7b25a419910f76e6
Gagan Vasanth <mynameisgagan20@gmail.com>
2023-05-15 22:15:30 +0530

    [dat-gui] addded lit-gui with types, created the folders and attached the function to control in UI

--- src/client/client.ts
+++ src/client/client.ts
@@ -4,7 +4,9 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"; // or
 // to show the stats to check the usability of your bwoser CPU and memory. You need to enable stats and this is only required
 // in development
 
-import Stats from 'three/examples/jsm/libs/stats.module';
+import Stats from "three/examples/jsm/libs/stats.module";
+
+import { GUI } from "lil-gui";
 
 const scene = new THREE.Scene();
 
@@ -27,9 +29,6 @@ pass the camera and dom element that you want to control at UI
 
 new OrbitControls(camera, renderer.domElement);
 
-const stats = new Stats();
-document.body.appendChild(stats.dom);
-
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({
   color: 0x00ff00,
@@ -44,14 +43,29 @@ function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
-  render();
+    render();
 }
+
+const stats = new Stats();
+document.body.appendChild(stats.dom);
+
+// add GUI for interacting with the models that we attach to in the UI
+
+const gui = new GUI();
+const cubeFolder = gui.addFolder("Cube"); // create a new folder and add the object to it
+cubeFolder.add(cube.rotation, "x", 0, Math.PI * 3); // added rotation of the cube
+cubeFolder.add(cube.rotation, "y", 0, Math.PI * 3);
+cubeFolder.add(cube.rotation, "z", 0, Math.PI * 3);
+
+const cameraFolder = gui.addFolder("camera");
+cameraFolder.add(camera.position, "z", 0, 10); // added position of the cube
+gui.open();
 
 function animate() {
   requestAnimationFrame(animate);
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  //   cube.rotation.x += 0.01;
+  //   cube.rotation.y += 0.01;
 
   render();
   stats.update();
